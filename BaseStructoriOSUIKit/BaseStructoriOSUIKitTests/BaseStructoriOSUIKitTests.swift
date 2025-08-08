20a7ef649468724fa32488560853f5848d85183c
Sahapap <us.sahapap@gmail.com>
2025-08-08 14:07:56 +0700

    Initializes the iOS UIKit project

---
+++ BaseStructoriOSUIKit/BaseStructoriOSUIKitTests/BaseStructoriOSUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaseStructoriOSUIKitTests.swift
+//  BaseStructoriOSUIKitTests
+//
+//  Created by sahapap on 8/8/2568 BE.
+//
+
+import Testing
+@testable import BaseStructoriOSUIKit
+
+struct BaseStructoriOSUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
