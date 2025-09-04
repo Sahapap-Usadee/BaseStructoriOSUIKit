6373b4ba48f6da06d9734cdc90ff867dd8023c39
sahapap_DGA <sahapap.usadee@dga.or.th>
2025-09-04 11:36:36 +0700

    update

--- BaseStructoriOSUIKit/BaseStructoriOSUIKit/Domain/Repositories/PokemonRepository.swift
+++ BaseStructoriOSUIKit/BaseStructoriOSUIKit/Domain/Repositories/PokemonRepository.swift
@@ -1,5 +1,5 @@
 //
-//  PokemonRepositoryImpl.swift
+//  PokemonRepository.swift
 //  BaseStructoriOSUIKit
 //
 //  Created by sahapap on 2/9/2568 BE.
@@ -15,40 +15,40 @@ protocol PokemonRepositoryProtocol {
 
 class PokemonRepositoryImpl: PokemonRepositoryProtocol {
     private let remoteDataSource: PokemonRemoteDataSourceProtocol
-    
+
     init(
         remoteDataSource: PokemonRemoteDataSourceProtocol
     ) {
         self.remoteDataSource = remoteDataSource
     }
-    
+
     func fetchPokemonList(limit: Int, offset: Int) async throws -> PokemonList {
         do {
             // Fetch from remote
             let dto = try await remoteDataSource.fetchPokemonList(limit: limit, offset: offset)
-            
+
             return dto.toDomain()
         } catch {
             throw error
         }
     }
-    
+
     func fetchPokemonDetail(id: Int) async throws -> Pokemon {
         do {
             // Fetch from remote
             let dto = try await remoteDataSource.fetchPokemonDetail(id: id)
-            
+
             return dto.toDomain()
         } catch {
             throw error
         }
     }
-    
+
     func fetchPokemonDetail(name: String) async throws -> Pokemon {
         do {
             // Fetch from remote
             let dto = try await remoteDataSource.fetchPokemonDetail(name: name)
-            
+
             return dto.toDomain()
         } catch {
             throw error
